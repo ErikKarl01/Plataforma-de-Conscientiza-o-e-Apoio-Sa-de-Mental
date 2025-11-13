57517db0992f6683049683865d77ca5fe031870b
curso_git <kekinpraxedes@gmail.com>
2025-11-13 16:51:34 -0300

    Feat: adicionados métodos de marcar um horário e remover um horário

--- backend/models/Psicologo.py
+++ backend/models/Psicologo.py
@@ -40,6 +40,58 @@ class Psicologo(Usuario):
             json.dump(dados, f)
             
         return jsonify({'mensagem': 'Consulta cadastrada com sucesso', 'consulta': novaConsulta})
+    
+    @staticmethod
+    def editarHorario():
+        dados_do_front = request.get_json()
+        
+        data = dados_do_front.get('data')
+        horarioDoFront = dados_do_front.get('horario')
+        
+        dataModificada = dados_do_front.get('dataModificada')
+        horarioModificado = dados_do_front.get('horarioModificado')
+        
+        with open('backend/data/consultas.json', 'f') as f:
+            dados = json.loads(f)
+        
+        for consulta in dados:
+            if consulta['data'] == data and consulta['horario'] == horarioDoFront:
+                consulta['data'] == dataModificada
+                consulta['horario'] == horarioModificado
+                
+                with open('backend/data/consultas.json', 'w') as f:
+                    json.dump(dados, f)
+                    
+                return jsonify({'mensagem': 'Consulta modiicada com sucesso', 'consulta': consulta})
+                    
+        with open('backend/data/consultas.json', 'w') as f:
+            json.dump(dados, f)
+            
+        return jsonify({'mensagem': 'Data e horário não encontrados'})
+    
+    @staticmethod
+    def excluirHorario():
+        dados_do_front = request.get_json()
+        
+        data = dados_do_front.get('data')
+        horarioDoFront = dados_do_front.get('horario')
+        
+        with open('backend/data/consultas.json', 'f') as f:
+            dados = json.loads(f)
+        
+        for consulta in dados:
+            if consulta['data'] == data and consulta['horario'] == horarioDoFront:
+                dados.remove(consulta)
+                
+                with open('backend/data/consultas.json', 'w') as f:
+                    json.dump(dados, f)
+                    
+                return jsonify({'mensagem': 'Consulta modiicada com sucesso', 'consulta': consulta})
+                    
+        with open('backend/data/consultas.json', 'w') as f:
+            json.dump(dados, f)
+            
+        return jsonify({'mensagem': 'Data e horário não encontrados'})               
         
 
 @app.route('/adicionarConsulta', methods=['POST'])
@@ -47,6 +99,15 @@ def marcar_consulta():
     consulta = Psicologo()
     return consulta.adicionarConsulta()
 
+@app.route('/modificarConsulta', methods=['POST'])
+def editar_horario():
+    consulta = Psicologo()
+    return consulta.editarHorario()
+
+@app.route('/removerConsulta', methods=['POST'])
+def excluir_horario():
+    consulta = Psicologo()
+    return consulta.excluirHorario()
 
 if __name__ == '__main__':
     app.run(debug=True)
