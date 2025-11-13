af22a568be8806ad8a3264c35603b23abc5f625d
curso_git <kekinpraxedes@gmail.com>
2025-11-13 16:06:45 -0300

    Criação de classe pau Usuario e classe filha psicólogo (separação do arquivo Usuario em duas partes)

--- backend/models/Psicologo.py
+++ backend/models/Psicologo.py
@@ -1,59 +1,24 @@
+from Usuario import Usuario
 from flask import Flask, request, jsonify
 import json
 import os
 
 app = Flask(__name__)
-
-if not os.path.exists('backend/data/usuarios.json'):
-    with open('backend/data/usuarios.json', 'w') as f:
-        json.dump([], f)
         
 if not os.path.exists('backend/data/consultas.json'):
     with open('backend/data/consultas.json', 'w') as f:
         json.dump([], f)
 
-class Psicologo:
-    id: str
-    nome: str
-    email: str
-    senha: str
-    telefone: str
+class Psicologo(Usuario):
     crp: str
     
-    def __init__(self, nome: str, email: str, senha: str, telefone: str, crp: str):
-        self.nome = nome
-        self.email = email
-        self.senha = senha
-        self.telefone = telefone
+    def __init__(self, nome, email, senha, telefone, crp):
+        super().__init__(nome, email, senha, telefone)
         self.crp = crp
-        
-    #Esse método permite pegar dados do front e adicionar a um arquivo json usado como banco de dados  
-    #Esse método cadastra um usuário  
-    def cadastrar(self):
-        dados_do_front = request.get_json()
-        
-        self.nome = dados_do_front.get('nome')
-        self.email = dados_do_front.get('email')
-        self.senha = dados_do_front.get('senha')
-        self.telefone = dados_do_front.get('telefone')
-        self.crp = dados_do_front.get('crp')
-        
-        novo_usuario = {'nome': self.nome, 'email': self.email, 'senha': self.senha, 'telefone': self.telefone, 'crp': self.crp}
-        
-        with open('backend/data/usuarios.json', 'r') as f:
-            dados = json.load(f)
-            
-        self.id = len(dados) - 1
-        novo_usuario['id'] = self.id    
-        dados.append(novo_usuario)
-        
-        with open('backend/data/usuarios.json', 'w') as f:
-            json.dump(dados, f)
-            
-        return jsonify({'mensagem': 'Usuário salvo comsucesso', 'usuario': novo_usuario})
     
     #Esse método permite pegar dados do front e adicionar a um arquivo json usado como banco de dados  
     #Esse método permite o psicólogo marcar uma consulta
+    @staticmethod
     def adicionarConsulta ():
         dados_do_front = request.get_json()
         
@@ -67,7 +32,7 @@ class Psicologo:
         with open('backend/data/consultas.json', 'r') as f:
             dados = json.load(f)
         
-        idConsulta = len(dados) - 1
+        idConsulta = len(dados) + 1
         novaConsulta['id'] = idConsulta
         dados.append(novaConsulta)
         
@@ -76,15 +41,6 @@ class Psicologo:
             
         return jsonify({'mensagem': 'Consulta cadastrada com sucesso', 'consulta': novaConsulta})
         
-        
-        
-        
-    
-    
-@app.route('/cadastrarPsicologo', methods=['POST'])
-def cadastrar_usuario():
-    usuario = Psicologo('', '', '', '', '')
-    return usuario.cadastrar()
 
 @app.route('/adicionarConsulta', methods=['POST'])
 def marcar_consulta():
